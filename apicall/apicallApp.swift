01cddbd2acaae2c50c5279fe9d662298ca994663
Tommer <souirtommer@gmail.com>
2023-06-07 21:46:26 +0800

    Initial Commit

---
+++ apicall/apicallApp.swift
@@ -0,0 +1,17 @@
+//
+//  apicallApp.swift
+//  apicall
+//
+//  Created by ST SE on 7/6/2023.
+//
+
+import SwiftUI
+
+@main
+struct apicallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
