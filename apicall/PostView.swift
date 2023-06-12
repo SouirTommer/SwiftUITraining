65066d6859199483870c47467fc88bbc8f824344
Tommer <souirtommer@gmail.com>
2023-06-12 12:48:32 +0800

    Add title, text and modify button style in PostView

--- apicall/PostView.swift
+++ apicall/PostView.swift
@@ -13,20 +13,41 @@ struct PostView: View {
     @State var stream = ""
     @State var showAlert = false
     var body: some View {
-        VStack{
-            TextField("name", text: $name)
-                .textFieldStyle(.roundedBorder)
-            TextField("stream", text: $stream)
-                .textFieldStyle(.roundedBorder)
-            Button(action: {
-                api.post(name: name, stream: stream)
-                showAlert = true
-            }, label: {
-                Text("Add")
-            })
-            .alert("Added", isPresented: $showAlert){}
-        }.padding()
         
+            VStack{
+                Text("WorldSkill - POST")
+                    .font(.largeTitle)
+                    .bold()
+                    .padding()
+                    .padding(.bottom, 30)
+                
+                    Text("Name:")
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                
+                
+                TextField("your name...", text: $name)
+                    .textFieldStyle(.roundedBorder)
+                
+                
+                Text("Stream:")
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                TextField("stream input...", text: $stream)
+                    .textFieldStyle(.roundedBorder)
+                
+                Button(action: {
+                    api.post(name: name, stream: stream)
+                    showAlert = true
+                }, label: {
+                    Text("Add")
+                })
+                .alert("Added", isPresented: $showAlert){}
+                .frame(width: 200)
+                .buttonBorderShape(.automatic)
+                .padding(.top, 10)
+                .font(.title3)
+                .buttonStyle(.borderedProminent)
+                
+            }.padding()
     }
     
 }
